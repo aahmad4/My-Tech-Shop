31fdc19ad82f1b453b0c13abda14dff764cd40e4
Ali Ahmad <alislearning123@gmail.com>
2021-01-25 00:07:45 -0500

    Create Model Files For Database & Create Dummy Data Files For Seeding

---
+++ server/data/users.js
@@ -0,0 +1,18 @@
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: xxxx,
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: xxxx,
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: xxxx,
+  },
+];
