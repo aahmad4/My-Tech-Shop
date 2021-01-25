96548ebc13b0feba5c0c278f6ab89831afcda748
Ali Ahmad <alislearning123@gmail.com>
2021-01-25 00:22:03 -0500

    Built & Completed Data Seeder File

--- server/data/users.js
+++ server/data/users.js
@@ -1,18 +1,22 @@
+import bcrypt from "bcryptjs";
+
 const users = [
   {
     name: "Admin User",
     email: "admin@example.com",
-    password: xxxx,
+    password: bcrypt.hashSync("password", 10),
     isAdmin: true,
   },
   {
     name: "John Doe",
     email: "john@example.com",
-    password: xxxx,
+    password: bcrypt.hashSync("password", 10),
   },
   {
     name: "Jane Doe",
     email: "jane@example.com",
-    password: xxxx,
+    password: bcrypt.hashSync("password", 10),
   },
 ];
+
+export default users;
