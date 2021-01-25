b5579caee58e8f3e4fa8838e4c4475b5c0a98b32
Ali Ahmad <alislearning123@gmail.com>
2021-01-24 22:49:22 -0500

    Create Scripts To Run Client & Server Concurrently

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -6,13 +6,13 @@ import axios from "axios";
 export default function HomeScreen() {
   const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    const { data } = await axios.get("/api/products");
-
-    setProducts(data);
-  };
-
   useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
     fetchProducts();
   }, []);
 
--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -7,15 +7,15 @@ import axios from "axios";
 export default function ProductScreen({ match }) {
   const [product, setProduct] = useState({});
 
-  const fetchProduct = async () => {
-    const { data } = await axios.get(`/api/products/${match.params.id}`);
-
-    setProduct(data);
-  };
-
   useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+
     fetchProduct();
-  });
+  }, [match]);
 
   return (
     <>
