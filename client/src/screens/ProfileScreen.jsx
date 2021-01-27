e586f2938d304ff8ea166c6e5983025433fa667d
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 17:49:44 -0500

    Create User Update Detail Constants, Action, Reducer, & Functionality In Profile Screen

--- client/src/screens/ProfileScreen.jsx
+++ client/src/screens/ProfileScreen.jsx
@@ -3,7 +3,7 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
 
 export default function ProfileScreen({ history }) {
   const [name, setName] = useState("");
@@ -19,6 +19,9 @@ export default function ProfileScreen({ history }) {
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
   useEffect(() => {
     if (!userInfo) {
@@ -38,6 +41,7 @@ export default function ProfileScreen({ history }) {
     if (password !== confirmPassword) {
       setMessage("Passwords do not match!");
     } else {
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
     }
   };
 
@@ -47,6 +51,7 @@ export default function ProfileScreen({ history }) {
         <h2>User Profile</h2>
         {message && <Message variant="danger">{message}</Message>}
         {error && <Message variant="danger">{error}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId="name">
