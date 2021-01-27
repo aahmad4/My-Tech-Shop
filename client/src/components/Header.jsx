8e3ee4c7bcd588a83ce79e71a6adfcc38c0f3a89
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 03:15:13 -0500

    Implement Logout Functionality & Change Header Based On Current User State

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { logout } from "../actions/userActions";
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
@@ -17,9 +29,20 @@ export default function Header() {
               <LinkContainer to="/cart">
                 <Nav.Link>🛒 Cart</Nav.Link>
               </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>👤 Sign In</Nav.Link>
-              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to="/login">
+                  <Nav.Link>👤 Sign In</Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
