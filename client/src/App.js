134fc119431974edd70b207f2afd24bb37f5ce1b
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 17:31:26 -0500

    Add Profile Screen & Create Get User Detail Constants, Action, & Reducer

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Container>
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/" component={HomeScreen} exact />
