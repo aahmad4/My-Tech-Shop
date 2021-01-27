12491c123bac482ce9d9d7e78c628d8a226bc606
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 01:43:23 -0500

    Add User Registration Controller & Route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,39 @@
 import asyncHandler from "express-async-handler";
-import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -11,11 +43,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email: email });
 
-  const matchPassword = async (enteredPassword) => {
-    return await bcrypt.compare(enteredPassword, user.password);
-  };
-
-  if (user && (await matchPassword(password))) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -48,4 +76,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { registerUser, authUser, getUserProfile };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  registerUser,
+  authUser,
+  getUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/").post(registerUser);
 router.route("/login").post(authUser);
 router.route("/profile").get(protect, getUserProfile);
 
