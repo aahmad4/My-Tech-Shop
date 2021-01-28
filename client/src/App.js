2cb6999f9808c939e5b1fd2630d9dc35e7261c1d
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 22:23:36 -0500

    Create Order Controller, Route, Constants, Action, Reducer, & Implement Functionality in PlaceOrderScreen Component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Container>
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/payment" component={PaymentScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/profile" component={ProfileScreen} />
