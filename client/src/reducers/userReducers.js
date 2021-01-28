3ecc7513b8ddc1b9e29568dedbcc0608d7a69be2
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 19:05:26 -0500

    Create Shipping Address Screen With Shipping Cart Constant, Action, & Reducer

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -12,7 +12,6 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
