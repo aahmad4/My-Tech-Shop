1ee7f782bad1c2bd6c146b9bd81047ea0e748bdf
Ali Ahmad <alislearning123@gmail.com>
2021-01-27 19:11:06 -0500

    Create Checkout Steps Nav Component & Add To Shipping Screen

--- client/src/screens/ShippingScreen.jsx
+++ client/src/screens/ShippingScreen.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 
 export default function ShippingScreen({ history }) {
@@ -24,6 +25,7 @@ export default function ShippingScreen({ history }) {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
