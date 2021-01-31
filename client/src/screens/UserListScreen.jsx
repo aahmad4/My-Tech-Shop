1b94442598c8c0cbd1c127b6fcdef3f5287ff163
Ali Ahmad <alislearning123@gmail.com>
2021-01-30 21:30:53 -0500

    Create User Delete Constants, Action, Reducer, & Implement Dispatcher On User List Screen

--- client/src/screens/UserListScreen.jsx
+++ client/src/screens/UserListScreen.jsx
@@ -4,7 +4,7 @@ import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 
 export default function UserListScreen({ history }) {
   const dispatch = useDispatch();
@@ -15,16 +15,21 @@ export default function UserListScreen({ history }) {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       history.push("/login");
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log(id);
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteUser(id));
+    }
   };
 
   return (
