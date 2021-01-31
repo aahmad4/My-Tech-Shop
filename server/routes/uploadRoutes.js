0e9cb713249a4604417837a807ab0061dae8fd6f
Ali Ahmad <alislearning123@gmail.com>
2021-01-31 18:44:03 -0500

    Add Cloudinary Image Integration With Server Route & Implementation On Client In Product Edit Screen

---
+++ server/routes/uploadRoutes.js
@@ -0,0 +1,54 @@
+import express from "express";
+const router = express.Router();
+import path from "path";
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+
+dotenv.config();
+
+const cloud = cloudinary.v2;
+
+cloud.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloud,
+  params: {
+    folder: "images",
+    // transformation: '540x430pad',
+    public_id: (req, file) =>
+      `${file.originalname.split(".")[0]}-${Date.now()}`,
+  },
+});
+
+const checkFileType = (file, cb) => {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(
+    path.extname(file.originalname).toLocaleLowerCase()
+  );
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send(req.file.path);
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
