c46175342d13f0152134d05cc60ce112433d9918
Ali Ahmad <alislearning123@gmail.com>
2021-01-30 20:15:13 -0500

    Create Admin Controller & Route So That They Can View All Users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,10 +5,11 @@ import {
   authUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, isAdmin, getUsers);
 router.route("/login").post(authUser);
 router
   .route("/profile")
