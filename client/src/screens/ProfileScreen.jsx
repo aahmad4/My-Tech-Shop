afbf6f4b31905a5d157f1dd4e7a7404ca1f165cc
Ali Ahmad <alislearning123@gmail.com>
2021-01-30 22:49:21 -0500

    Add Dollars Sign Next To Total Prices For Order List On Profile Screen

--- client/src/screens/ProfileScreen.jsx
+++ client/src/screens/ProfileScreen.jsx
@@ -124,7 +124,7 @@ export default function ProfileScreen({ history }) {
                 <tr key={order._id}>
                   <td>{order._id}</td>
                   <td>{order.createdAt.substring(0, 10)}</td>
-                  <td>{order.totalPrice}</td>
+                  <td>${order.totalPrice}</td>
                   <td>
                     {order.isPaid ? (
                       order.paidAt.substring(0, 10)
