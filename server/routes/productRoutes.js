994f717d997fc3af69255ddfef9870535e39f69b
Ali Ahmad <alislearning123@gmail.com>
2021-01-31 17:22:01 -0500

    Add Product Controller & Route For Creating / Updating Products. Added Sample Image As A Default For New Products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,13 +4,16 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .delete(protect, isAdmin, deleteProduct);
+  .delete(protect, isAdmin, deleteProduct)
+  .put(protect, isAdmin, updateProduct);
 
 export default router;
