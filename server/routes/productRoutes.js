3fe2cfcc68166e8b666ec208a466fbdd830a0693
Ali Ahmad <alislearning123@gmail.com>
2021-01-30 23:29:16 -0500

    Add Delete Product Route & Controller In Server

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct);
 
 export default router;
