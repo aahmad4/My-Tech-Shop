0129ac041667311dcbe48333cc9d56c243266414
Ali Ahmad <alislearning123@gmail.com>
2021-01-31 17:36:19 -0500

    Add Create Product Constants, Reducer, Action, & Implement Functionality In Product List Screen

--- client/src/screens/ProductListScreen.jsx
+++ client/src/screens/ProductListScreen.jsx
@@ -4,7 +4,12 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listProducts, deleteProduct } from "../actions/productActions";
+import {
+  listProducts,
+  deleteProduct,
+  createProduct,
+} from "../actions/productActions";
+import { PRODUCT_CREATE_RESET } from "../constants/productConstants";
 
 export default function ProductListScreen({ history, match }) {
   const dispatch = useDispatch();
@@ -22,13 +27,34 @@ export default function ProductListScreen({ history, match }) {
     success: successDelete,
   } = productDelete;
 
+  const productCreate = useSelector((state) => state.productCreate);
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    product: createdProduct,
+  } = productCreate;
+
   useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listProducts());
-    } else {
+    dispatch({ type: PRODUCT_CREATE_RESET });
+
+    if (!userInfo.isAdmin) {
       history.push("/login");
     }
-  }, [dispatch, history, userInfo, successDelete]);
+
+    if (successCreate) {
+      history.push(`/admin/product/${createdProduct._id}/edit`);
+    } else {
+      dispatch(listProducts());
+    }
+  }, [
+    dispatch,
+    history,
+    userInfo,
+    successDelete,
+    successCreate,
+    createdProduct,
+  ]);
 
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure?")) {
@@ -36,8 +62,8 @@ export default function ProductListScreen({ history, match }) {
     }
   };
 
-  const createProductHandler = (product) => {
-    console.log("create");
+  const createProductHandler = () => {
+    dispatch(createProduct());
   };
 
   return (
@@ -54,6 +80,8 @@ export default function ProductListScreen({ history, match }) {
       </Row>
       {loadingDelete && <Loader />}
       {errorDelete && <Message variant="danger">{errorDelete}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant="danger">{errorCreate}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
