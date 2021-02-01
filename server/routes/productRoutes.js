14edd1d430e2ed712ef236ef83ff5c58649cdf6e
Ali Ahmad <alislearning123@gmail.com>
2021-01-31 20:45:58 -0500

    Install Morgan For API Request Logging & Add Product Review Creation Route / Controller

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -6,10 +6,12 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, isAdmin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router
   .route("/:id")
   .get(getProductById)
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
@@ -13,6 +14,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
