f33d0c0cb6152775db81ba078d7ddcb052d0c58c
Ali Ahmad <alislearning123@gmail.com>
2021-01-31 19:58:11 -0500

    Create Order List Controller / Route In Sever & Create Order List Constants, Action, & Reducer For Client

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -5,10 +5,11 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
 } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, addOrderItems);
+router.route("/").post(protect, addOrderItems).get(protect, isAdmin, getOrders);
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
