6aaea822270a516eda092b362b91868a4de8f5c4
Ali Ahmad <alislearning123@gmail.com>
2021-01-31 23:54:31 -0500

    Add Search Bar Component For Filtering Products & Implement Functionality In Get Products Controller

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -6,15 +6,17 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { listProducts } from "../actions/productActions";
 
-export default function HomeScreen() {
+export default function HomeScreen({ match }) {
+  const keyword = match.params.keyword;
+
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <>
